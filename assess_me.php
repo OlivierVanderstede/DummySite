9df95278af169e20f72c95e7b63eafeb412f4bab
Olivier Vanderstede <otje@telenet.be>
2017-11-25 12:09:02 +0100

    Dummy first check in

---
+++ assess_me.php
@@ -0,0 +1,12 @@
+<?php
+    $paginaTitel = "Assess me";
+    include('header.html');
+?>
+
+<div class="content">
+    <?php echo $_SERVER['SERVER_NAME'];?>
+</div>
+
+<?php
+    include('footer.html');
+?>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    $paginaTitel = "Index";
+    include('header.html');
+?>
+
+<div class="content">
+    The only thing it does is link this page to a <a href="assess_me.php">second</a> one
+</div>
+<?php
+    include('footer.html');
+?>
