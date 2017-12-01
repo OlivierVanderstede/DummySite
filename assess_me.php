908d577e9d603cc494da47adfc31c4d3a9a8d5fa
Olivier Vanderstede <otje@telenet.be>
2017-12-01 16:20:14 +0100

    removed assess_me html shizzle

--- assess_me.php
+++ assess_me.php
@@ -1,12 +1,1 @@
-<?php
-    $paginaTitel = "Assess me";
-    include('header.html');
-?>
-
-<div class="content">
-    <?php echo $_SERVER['SERVER_NAME'];?>
-</div>
-
-<?php
-    include('footer.html');
-?>
+<?php echo $_SERVER['SERVER_NAME'];?>
