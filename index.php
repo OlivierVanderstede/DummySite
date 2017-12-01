d884d5ca75b00697d6f476793963bd83dc00a844
Olivier Vanderstede <otje@telenet.be>
2017-12-01 19:12:50 +0100

    removed php links

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
 <h1 class="title">I'm one with the Force. The Force is with me.</h1>
 <div class="content">
-    <a class="btn btn-default btn-lg" href="assess_me.php">Assess me!</a>
+    <a class="btn btn-default btn-lg" href="assess_me">Assess me!</a>
 </div>
 <?php
     include('footer.html');
